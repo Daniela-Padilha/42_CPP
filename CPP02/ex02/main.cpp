8dd95489d05e61210ddb630e36c41ad133bd87fd
Daniela-Padilha <ddo-carm@LAPTOP-REDJ8VVL.localdomain>
2025-07-17 22:27:08 +0100

    started ex02

---
+++ CPP02/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddo-carm <ddo-carm@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/17 22:04:04 by ddo-carm          #+#    #+#             */
+/*   Updated: 2025/07/17 22:06:18 by ddo-carm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "inc/Fixed.hpp"
+
+int main( void )
+{
+	Fixed	a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max( a, b ) << std::endl;
+
+ 	return 0;
+}
