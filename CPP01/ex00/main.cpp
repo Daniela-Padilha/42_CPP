9d7468cc77e4c5ef6db3539f1d7ec424d4d19812
Daniela Sofia Do Carmo Padilha <ddo-carm@c2r3s8.42porto.com>
2025-07-23 18:53:01 +0100

    Improved testing in ex00

--- CPP01/ex00/main.cpp
+++ CPP01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ddo-carm <ddo-carm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 13:19:52 by ddo-carm          #+#    #+#             */
-/*   Updated: 2025/07/13 17:14:49 by ddo-carm         ###   ########.fr       */
+/*   Updated: 2025/07/23 18:51:24 by ddo-carm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ int	main(void)
 	Zombie 	stackzombie("Brian");
 	std::string str = "Martha";
 	
-	std::cout << "\033[36;1m " << str << " is a zombie from the heap: \033[0m" << std::endl;
+	std::cout << "\033[36;1m" << str << " is a zombie from the heap: \033[0m" << std::endl;
 	randomChump(str);
 	std::cout << std::endl;
-	std::cout << "\033[36;1m " << stackzombie.getName()
+	std::cout << "\033[36;1m" << stackzombie.getName()
 		<< " is a zombie from the stack: \033[0m" << std::endl;
 	stackzombie.announce();
 	return (0);
