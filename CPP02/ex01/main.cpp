cc8b15138165c3afe721a724d7d9ab1f1560ae8b
Daniela Padilha <danielasofiapadilha@gmail.com>
2025-07-29 18:55:05 +0100

    Update main.cpp

--- CPP02/ex01/main.cpp
+++ CPP02/ex01/main.cpp
@@ -14,21 +14,38 @@
 
 int main( void )
 {
+	std::cout << CYA "	OBJECT: a" RES << std::endl;
 	Fixed		a;
+	std::cout << std::endl;
+
+	std::cout << CYA "	OBJECT: b" RES << std::endl;
 	Fixed const b( 10 );
+	std::cout << std::endl;
+	
+	std::cout << CYA "	OBJECT: c" RES << std::endl;
 	Fixed const c( 42.42f );
+	std::cout << std::endl;
+	
+	std::cout << CYA "	OBJECT: d" RES << std::endl;
 	Fixed const d( b );
 
+	std::cout << std::endl;
 	a = Fixed( 1234.4321f );
+	std::cout << std::endl;
 
+	std::cout << CYA "	VALUES" RES << std::endl;
 	std::cout << "a is " << a << std::endl;
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
 	std::cout << "d is " << d << std::endl;
+	std::cout << std::endl;
 
+	std::cout << CYA "	Int conversion" RES << std::endl;
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << std::endl;
+	
 	return 0;
 }
