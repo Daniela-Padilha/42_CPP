d3633b09634b19236d359b4ae49659ad6e6c365b
Daniela Sofia Do Carmo Padilha <ddo-carm@c2r3s9.42porto.com>
2025-10-23 18:34:57 +0100

    started ex00

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -6,9 +6,45 @@
 /*   By: ddo-carm <ddo-carm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 17:22:52 by ddo-carm          #+#    #+#             */
-/*   Updated: 2025/10/22 17:23:22 by ddo-carm         ###   ########.fr       */
+/*   Updated: 2025/10/23 18:33:20 by ddo-carm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/ScalarConverter.hpp"
 
+/*------------------------------Constructors---------------------------------*/
+
+ScalarConverter::ScalarConverter()
+{
+	std::cout << BGRN "Scalar Converter was created\n" RES;
+}
+
+ScalarConverter::ScalarConverter(ScalarConverter& const other)
+{
+	*this = other;
+	std::cout << BYEL "Scalar Converter copy constructor was called\n" RES;
+}
+
+/*-------------------------------Destructors---------------------------------*/
+
+ScalarConverter::~ScalarConverter()
+{
+	std::cout << BRED "Scalar Converter was destroyed\n" RES;
+}
+
+/*------------------------------Operators------------------------------------*/
+
+ScalarConverter& ScalarConverter::operator = (ScalarConverter& const other)
+{
+	std::cout << CYA "Scalar Converter assignment operator was alled\n" RES;
+	if (this != &other)
+		*this = other;
+	return (*this);
+}
+
+/*-----------------------------Member Functions------------------------------*/
+
+void ScalarConverter::convert(const std::string& value)
+{
+	
+}
--- CPP06/ex00/inc/ScalarConverter.hpp
+++ CPP06/ex00/inc/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: ddo-carm <ddo-carm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 17:22:01 by ddo-carm          #+#    #+#             */
-/*   Updated: 2025/10/22 17:34:38 by ddo-carm         ###   ########.fr       */
+/*   Updated: 2025/10/23 17:04:31 by ddo-carm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define SCALARCONVERTER_HPP
 
 # include <string>
+# include <iostream>
 
 class ScalarConverter {
 	private:
@@ -23,7 +24,16 @@ class ScalarConverter {
 
 		ScalarConverter& operator = (ScalarConverter& const other);
 	public:
-		convert(std::string value);
+		static void convert(const std::string& value);
 };
+
+// -----------Colours-----------
+
+# define RES "\033[0m"
+# define BRED "\033[31;1m"
+# define BGRN "\033[32;1m"
+# define BYEL "\033[33;1m"
+# define CYA "\033[0;36m"
+# define BMAG "\033[35;1m"
 
 #endif
