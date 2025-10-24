ccc24858c34213c29c1e7c8d5ac8be422a245520
Daniela-Padilha <danielasofiapadilha@gmail.com>
2025-10-24 22:36:28 +0100

    started ex01

---
+++ CPP07/ex01/inc/iter.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddo-carm <ddo-carm@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/24 22:30:47 by ddo-carm          #+#    #+#             */
+/*   Updated: 2025/10/24 22:36:16 by ddo-carm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+template <typename A, typename B> void iter(A *addr, const int size, B func)
+{
+	
+}
+
+#endif
