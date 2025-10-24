98f191181ac73bcb151976d85da4b1e8d228bf71
Daniela-Padilha <danielasofiapadilha@gmail.com>
2025-10-25 00:26:58 +0100

    did inter, working on main

--- CPP07/ex01/inc/iter.hpp
+++ CPP07/ex01/inc/iter.hpp
@@ -6,16 +6,38 @@
 /*   By: ddo-carm <ddo-carm@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 22:30:47 by ddo-carm          #+#    #+#             */
-/*   Updated: 2025/10/24 22:36:16 by ddo-carm         ###   ########.fr       */
+/*   Updated: 2025/10/24 23:45:37 by ddo-carm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
-template <typename A, typename B> void iter(A *addr, const int size, B func)
+# include <iostream>
+
+template <typename A, typename B> void iter(A *arr, const int size, B func)
 {
-	
+	if (size <= 0)
+	{
+		std::cout << BRED "Error: invalid size" RES << std::endl;
+		return ;
+	}
+	if (!arr)
+	{
+		std::cout << BRED "Error: invalid pointer" RES << std::endl;
+		return ;
+	}
+	for (int i = 0; i < size, i++)
+		func(arr[i]);
 }
+
+// -----------Colours-----------
+
+# define RES "\033[0m"
+# define BRED "\033[31;1m"
+# define BGRN "\033[32;1m"
+# define BYEL "\033[33;1m"
+# define CYA "\033[0;36m"
+# define BMAG "\033[35;1m"
 
 #endif
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -6,13 +6,19 @@
 /*   By: ddo-carm <ddo-carm@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 22:30:44 by ddo-carm          #+#    #+#             */
-/*   Updated: 2025/10/24 22:31:42 by ddo-carm         ###   ########.fr       */
+/*   Updated: 2025/10/24 23:46:34 by ddo-carm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/iter.hpp"
+#include <string>
 
 int main()
 {
+	int intArr[] = {1, 2, 3, 4, 5, 6};
+	char charArr[] = {'a', 'b', 'c'};
+	std::string strArr[] = {"one", "two", "three", "four"};
 
+	
+	return (0);
 }
